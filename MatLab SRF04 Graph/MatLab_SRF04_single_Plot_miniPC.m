244a9ba7f1bb4cdf9684a91ce5a314c8093f4322
Luís Afonso <luis.r.afonso@gmail.com>
2016-06-28 21:48:42 +0100

    final srf04 graph
    final version of sf04 graph

---
+++ MatLab SRF04 Graph/MatLab_SRF04_single_Plot_miniPC.m
@@ -0,0 +1,73 @@
+%=================================================================
+% Creates a real time graph from 1 data received from serial
+% Each data packet should be 1 single byte
+% Change the COM name to the one you want in the function serial()
+%=================================================================
+
+% delete all serial ports from memory 
+% important, if you the code is stopped without closing and deleting the
+% used COM, you need to do this to open it again
+delete(instrfindall);
+
+% Init and open the serial port
+s = serial('COM3', 'baudrate', 115200);
+fopen(s);
+
+%close all open figures
+close ALL
+
+points = 100; %number of points on the graph at all times
+data_period = 50; %data period in milliseconds 
+%x will be the time axis. The time between points is defined by
+%"data_period"
+x = linspace(0,points*data_period, points);
+
+%y will hold the distance, for now all values will be 0 and will have the
+%size defined by "points"
+y = linspace(0,0,points);
+
+% draw a area graph with parameter x and y. Save the return value so we can
+% edit the graph later. You can use "plot" instead of "area", I just liked
+% the aspect better.
+figure;
+lh = area(x,y);
+
+% set the x and y axis limits to [0, points*data_period] and [0, 255]
+% respectively
+axis([0,points*data_period,0,255]);
+shg;
+
+key = get(gcf,'CurrentKey'); %get the key currently pressed
+while ( strcmp(key, 's') == 0) %this while will stop if you press the "s" key
+
+    %block until there's at least 1 byte available to read
+    while s.BytesAvailable == 0 
+    end
+    
+    %push the all the values to the left of the graph 
+    for k = 1:1:points-1
+       y(k) = y(k+1);
+    end
+
+    %read and place value in the right of the graph
+    y(points) = fread(s,1);
+    
+    %save the value in distance without a ";" so we can read the number in
+    %console
+    distance = y(points)
+ 
+  
+    % edit just the data for the y axis on the graph. This is much, much
+    % faster than ploting everything over and over again
+    set(lh, 'YData',y);   
+    %request the plot to be updated
+    drawnow;
+
+    
+    key = get(gcf,'CurrentKey'); %get the key currently pressed
+end
+    
+close ALL %close all open figures
+fclose(s); %close serial port
+delete(s); %remove serial port from memory
+clear s; %erase the variable "s"
--- MatLab SRF04 Graph/SRF04_Graph_Arduino/SRF04_Graph_Arduino.ino
+++ MatLab SRF04 Graph/SRF04_Graph_Arduino/SRF04_Graph_Arduino.ino
@@ -1,18 +1,34 @@
-#define echoPin PB_2
-#define trigPin PE_0
+/*
+
+  This is a code to send by serial the distance measured by a HC-SR04 or SRF04 to a MatLab program.
+  MatLab program: "MatLab_SRF04_single_Plot.m"
+  
+  Made by Luís Afonso
+
+*/
+
+
+#define echoPin PB_2 //change pin to the number you want
+#define trigPin PE_0 //change pin to the number you want
+
+#define BUTTON_PIN PF_0 //change pin to the number you want
 
 void setup()
 {
-  // put your setup code here, to run once:
+
   Serial.begin(115200);
-  pinMode(PF_0, INPUT_PULLUP);
+  
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  while(digitalRead(BUTTON_PIN) == HIGH);
+  
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
 }
 
-int j;
+
 void loop()
 {
+  /* Get the distance from the sensor like always */
   long duration, distance;
   digitalWrite(trigPin, LOW);  // Added this line
   delayMicroseconds(2); // Added this line
@@ -23,7 +39,14 @@ void loop()
   duration = pulseIn(echoPin, HIGH);
   distance = (duration) / 58;
 
+  //for this example we just use 1 byte so we have to keep the value limited to 0-255
+  if(distance > 255)
+    distance = 255;
+    
+  //Send the distance in 1 byte (actually sends for example 156, not '1' '5' and '6')
   Serial.write(distance);
+  
+  
   delay(50);
 
 }
